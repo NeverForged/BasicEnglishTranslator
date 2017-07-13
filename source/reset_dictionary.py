3b54e4aba96310dcef136612367b1d8d6ee7cace
Darin LaSota <Darin.LaSota@gmail.com>
2017-07-13 06:41:09 -0700

    Use Google News

--- source/reset_dictionary.py
+++ source/reset_dictionary.py
@@ -28,7 +28,7 @@ def main():
     print 'making dictionary'
     s3 = time.clock()
     d = md.make_dict()
-    print '   ...took {:.3f}s - {}\n'.format(s3-s2, len(d))"
+    print '   ...took {:.3f}s - {}\n'.format(s3-s2, len(d))
 
     with open('../data/basic_english.pickle', 'wb') as handle:
         pickle.dump(d,handle, protocol=pickle.HIGHEST_PROTOCOL)
