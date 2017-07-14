517edb0c4e0c8aaae6e6cf1ab85ed67f59250d4c
Darin LaSota <Darin.LaSota@gmail.com>
2017-07-14 09:17:35 -0700

    Changes...

--- source/graph_model.py
+++ source/graph_model.py
@@ -7,6 +7,8 @@ from collections import defaultdict
 import cPickle as pickle
 import networkx as nx
 import nxpd as nxpd
+import sys
+
 
 def find_sims(model):
     '''
@@ -42,17 +44,16 @@ def find_sims(model):
                    ]
     start = time.clock()
     ret = defaultdict(list)
-    ylst = list('?!12345678"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r\x0b\x0c')
+    ylst = list('?!12345678"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r\x0b\x0c')
     for i, word in enumerate(model.vocab.keys()):
         if any(x in ylst for x in word):
             # weird one with punctuation, or a phrase
             pass
         else:
+            keys_length = float(len(model.vocab.keys()))
             # only use the ones I want to replace
-            # title case to factor out names, though it kills words like Kill
-            pos = pos_tag([word.title()])[0][1]
-            pos1 = pos_tag([word])[0][1]
-            if pos in make_simple or pos1 in make_simple:
+            pos = pos_tag([word])[0][1]
+            if pos in make_simple:
                 # it's the sort of thing we'd replace...
                 try:
                     lst = [a[0].lower() for a in model.most_similar(word.lower())
@@ -66,17 +67,17 @@ def find_sims(model):
                     for a in lst:
                         if pos_tag([a])[0][1] == pos:
                             nlst.append(a)
-                    if len(nlst) > 2:
-                        for i in xrange(int(len(nlst)/2)):
-                            rem = model.doesnt_match(nlst + [word])
-                            if rem == word:
-                                pass
-                            else:
-                                nlst.pop(nlst.index(rem))
+                    # avoid at least one opposite...
+                    if len(nlst) > 2: # but only if there are options
+                        rem = model.doesnt_match(nlst + [word])
+                        if rem == word:
+                            pass
+                        else:
+                            nlst.pop(nlst.index(rem))
                     ret[word] = nlst
-        if i % 100 == 0:
-            per = 100.0 * i/float(len(model.vocab.keys()))
-            print 'Get Connections {:.3f}%  \r'.format(per),
+            if i % 100 == 0:
+                per = 100.0 * i/keys_lenth
+                print 'Get Connections {:.2f}%  \r'.format(per),
 
     end = time.clock()
     print 'Dictionary took {:.2f}s'.format((end - start)/60.0)
@@ -131,11 +132,13 @@ def make_graph_model(d):
     Makes a graph model from a dictionary file.
     '''
     # now, to make lines for nx... edgelist
+    start = time.clock()
     lines = []
     for key in d.keys():
         for item in d[key]:
             lines.append('{} {}'.format(key, item))
     G = nx.parse_edgelist(lines)
+    print 'G took {:.2f}s'.format(time.clock() - start)
     return G, d
 
 def make_dictionary(G, input_d):
@@ -147,37 +150,41 @@ def make_dictionary(G, input_d):
     vocab = input_d.keys()
     vocab.sort()
     vocab.sort(key=len, reverse=True)
-    lst = [A for A in nx.connected_components(G) if any(x in vocab for x in A)]
-    # make only the subgraphs that connect to at least one of our words
-    Graphs = []
-    for A in lst:
-        Graphs.append(G.subgraph(A))
 
     start = time.clock()
     paths = defaultdict(list)
     # make a dictionary...
     for i, word in enumerate(vocab):
-        for graph in Graphs:
-            # this should only come up once per
-            if word in graph.nodes():
-                # each node...
-                for point in graph.nodes():
-                    # find path, replace if needed
-                    path = nx.shortest_path(G, point, word)
-                    if len(paths[point]) < len(path):
-                        paths[point] = path
+        try:
+            temp = nx.shortest_path(G, target=word)
+        except:
+            temp = {word:word}
+        for key in temp.keys():
+            length = len(paths[key])
+            length_n = len(temp[key])
+            if length == 0 or length < length_n:
+                paths[key] = temp[key]
         if i % 25 == 0:
             per = 100.0*i/float(len(vocab))
-            print '    Pathfinder: {:.3f}% \r'.format(per),
+            print '    Pathfinder: {:.0f}% \r'.format(per),
     print 'Paths Found, Took {:.2f}s'.format(time.clock() - start)
+    with open('../data/temp_paths.pickle', 'wb') as handle:
+            pickle.dump(paths, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     # now set our dictionary
     start = time.clock()
     for i, key in enumerate(paths.keys()):
-        pos = pos_tag([paths[key][-1]])
+        pos = pos_tag([paths[key][-1]])[0][1]
         # i still don't trust the pos tagging
-        if pos == pos_tag([paths[key][0]]):
+        # if pos == pos_tag([paths[key][0]]):
+        if type(paths[key]) != list:
+            input_d[key] = [paths[key], paths[key], pos]
+        elif len(paths[key]) >= 2:
             input_d[key] = [paths[key][-1], paths[key][1], pos]
+        else:
+            # self as word...?
+            input_d[key] = [list(paths[key])[0], list(paths[key])[0], pos]
+            print input_d[key]
         if i % 25  == 0:
             per = 100.0*i/float(len(paths))
             print '    Dictionary: {:.3f}% \r'.format(per),
@@ -186,12 +193,19 @@ def make_dictionary(G, input_d):
             pickle.dump(input_d, handle, protocol=pickle.HIGHEST_PROTOCOL)
     return input_d
 
-
 if __name__ == '__main__':
-    model = get_google()
+    if len(sys.argv) > 1:
+        if sys.argv[1].lower() == 'google'
+        model = get_google()
+    else
+        model = get_sentences_model()
     d = get_sims('test', model)
+    # d = {'word':['words', 'wordy', 'worded'], 'thing':['word', 'wizzy', 'wick', 'foo'], 'foo':['bar']}
     G, d = make_graph_model(d)
+    # print G.nodes()
     newd = pickle.load(open('../data/basic_english - Copy small.pickle',
                            'rb'))
     del model
-    make_dictionary(G, newd)
+    # newd = {'foo':['foo', 'foo', 'NN'], 'word':['word', 'word', 'NN']}
+    thed = make_dictionary(G, newd)
+    # print thed
