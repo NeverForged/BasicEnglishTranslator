19711424c0a3fa39c663ed03cb4b975a15e0682c
Darin LaSota <Darin.LaSota@gmail.com>
2017-07-31 19:10:04 -0700

    Testing something

--- source/app.py
+++ source/app.py
@@ -3,9 +3,16 @@ import cPickle as pickle
 from BasicEnglishTranslator import BasicEnglishTranslator
 from flask import Flask, render_template, request, jsonify
 
-
 app = Flask(__name__)
+@app.route('/process', methods=['POST'])
 
+@app.route('/draw/<specs>.png')
+def draw(specs):
+    n = len(specs)
+    f = '/static/images/neverforged_logo.png'
+    if n % 2 == 0:
+        f = '/static/images/neverforged_logo_w.png'
+    return redirect(f)
 
 # load page when someone hits the site
 @app.route('/', methods=['GET'])
@@ -35,7 +42,9 @@ def translate_text(text_input):
     '''
     Calls BasicEnglishTranslator and fits the text.
     '''
-    trans = BasicEnglishTranslator()
+    d_name = '../data/basic_english.pickle'
+    dictionary = pickle.load(open(d_name, "rb"))
+    trans = BasicEnglishTranslator(basic_dictionary=dictionary, verbose=True)
     return trans.fit(text_input)
 
 if __name__ == '__main__':
